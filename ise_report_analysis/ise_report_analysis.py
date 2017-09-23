2c642fb7723351b0509f33b1f42972ecd47e9c02
ZanabizR <31190681+brad93hunt@users.noreply.github.com>
2017-09-23 21:32:03 +0100

    Added separate class for queries

--- ise_report_analysis/ise_report_analysis.py
+++ ise_report_analysis/ise_report_analysis.py
@@ -8,7 +8,9 @@
 ### Notes: Problems to Solve
 ### - Assign table headers to variable to pass into table creation function
 ### - Assign column names to variable to pass into SELECT WHERE statements wher column name is return value
+###     * Solution - Assign values to variables which will be passed into the function when it is called. Investiagte * being used
 ### - Provide the ability to use a signle or multiple SELECT WHERE options in a class function
+### - Create another table from the output of a table query
 
 
 import csv
@@ -43,7 +45,12 @@ class database_manager(object):
                             """, write_db)
             self.conn.commit()
 
-    def query_db_table(self,table_name, column_name, value):
+    def succ_query_db_table(self,table_name, column_1, value_1, column_2, value_2):
+        self.cursor.execute('SELECT * FROM Raw_Data WHERE ? = ? AND NOT ? = ?', (column_1, value_1, column_2, value_2))
+        query_list = self.cursor.fetchall()
+        create_db_table(table_name, query_list)
+
+    def other_query_db_table(self,table_name, column_name, value):
         self.cursor.execute('SELECT * FROM Raw_Data WHERE ? = ?', (column_name, value))
         query_list = self.cursor.fetchall()
         create_db_table(table_name, query_list)
@@ -92,7 +99,9 @@ def main():
 
     # Run queries and create new tables based on query returns
     # e.g. Query Raw_Data for all successful authentications, create succ table from query of all successful authentications
-    database.query_db_table('Succ', 'SELECTED_AZN_PROFILE', 'Full_corporate_access')
+    blank = ''
+
+    #database.query_db_table('Succ', 'SELECTED_AZN_PROFILE', 'Full_corporate_access')
 
 
     # Close connection to database
